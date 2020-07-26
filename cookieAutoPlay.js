992b0c2a8d9878389a70d3b1899b353f589728a9
rahidz <rahidz2003@gmail.com>
2020-07-26 05:22:12 -0400

    Update cookieAutoPlay.js
    update from rawgit to jsdeliver

--- cookieAutoPlay.js
+++ cookieAutoPlay.js
@@ -5,6 +5,6 @@ var xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://api.github.com/repos/rahidz/CookieBot/releases/latest', false);
 xhr.send();
 var response = JSON.parse(xhr.responseText);
-var latest = "https://cdn.rawgit.com/rahidz/CookieBot/" + response.tag_name + "/cookieAutoPlayBeta.js"
+var latest = "https://cdn.jsdelivr.net/rahidz/CookieBot/" + response.tag_name + "/cookieAutoPlayBeta.js"
 console.log("### LOADING FROM " + latest); 
 Game.LoadMod(latest);
