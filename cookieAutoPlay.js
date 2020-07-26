8853c91ed2479d4feb54c0425c5e5f453cd4a6ca
rahidz <rahidz2003@gmail.com>
2020-07-26 04:53:18 -0400

    Add files via upload

---
+++ cookieAutoPlay.js
@@ -0,0 +1,10 @@
+//cookie bot: auto-play-through cookie clicker
+// Here, we just find the last stable release and load it.
+console.log("### COOKIE clicker STABLE - linking to latest release"); 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://api.github.com/repos/rahidz/CookieBot/releases/latest', false);
+xhr.send();
+var response = JSON.parse(xhr.responseText);
+var latest = "https://cdn.rawgit.com/rahidz/CookieBot/" + response.tag_name + "/cookieAutoPlayBeta.js"
+console.log("### LOADING FROM " + latest); 
+Game.LoadMod(latest);
